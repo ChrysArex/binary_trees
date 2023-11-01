aef120320a1dafb8888e264d69b15ae86f50dcf7
root <root@localhost.localdomain>
2023-11-01 11:53:22 +0100

    test

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts a node as the
+ * left-child of another node
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ * Return: return a pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL || parent == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	if (parent->left != NULL)
+		new->left = parent->left;
+	parent->left = new;
+	return (new);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+This repository is about binary trees. It contain various function to make
+operations on a binary tree such as:
+- create a node
+- insertion of node at the left/right position
+- delete a node/binary tree
+- checks if a node is a leaf
+- ...
